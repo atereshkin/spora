ab62f00b01ce1ce555464ac51e1b13cc973c4b64
Alexander Tereshkin <>
2025-12-22 17:17:31 +0200

    Minimal logging in pubsub

--- pubsub/src/main.rs
+++ pubsub/src/main.rs
@@ -6,6 +6,7 @@ use tokio::io::{
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Mutex;
 use clap::Parser;
+use log::{error, info};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -23,6 +24,7 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let args = Args::parse();
     let sub_listener = TcpListener::bind(("0.0.0.0", args.sub_port)).await.unwrap();
     let pub_listener = TcpListener::bind(("0.0.0.0", args.pub_port)).await.unwrap();
@@ -34,7 +36,11 @@ async fn main() {
     loop {
         let (socket, _) = sub_listener.accept().await.unwrap();
         let server = server.clone();
-        tokio::spawn(async move { server.handle_sub(socket).await });
+        tokio::spawn(async move {
+             if let Err(e) = server.handle_sub(socket).await {
+                 error!("Error while processing subscriber: {}", e);
+            }
+        });
     }
 }
 
@@ -52,54 +58,62 @@ impl PubServer {
             subscribers: Mutex::new(HashMap::new()),
         }
     }
-    async fn handle_sub(&self, mut socket: TcpStream) {
+    async fn handle_sub(&self, mut socket: TcpStream) -> std::io::Result<()> {
+        let peer_addr = socket.peer_addr()?;
         let (reader, writer) = socket.split();
         let mut reader = BufReader::new(reader);
         let mut writer = BufWriter::new(writer);
 
         let mut key = String::new();
-        reader.read_line(&mut key).await.unwrap();
-        print!("SUB: {}", key);
+        reader.read_line(&mut key).await?;
+        info!("SUB: {} {}", peer_addr, key);
 
         //TODO: we should check if subscriber is already there
         let mut subscribers = self.subscribers.lock().await;
 
-        writer.write_all(format!("{}:{}\n", self.pub_host, self.pub_port).as_ref()).await.unwrap();
-        writer.flush().await.unwrap();
+        writer.write_all(format!("{}:{}\n", self.pub_host, self.pub_port).as_ref()).await?;
+        writer.flush().await?;
         subscribers.insert(key, socket);
+        Ok(())
     }
 
     async fn listen_pub(server: Arc<PubServer>, listener: TcpListener) {
         loop {
             let (socket, _) = listener.accept().await.unwrap();
             let server = server.clone();
-            tokio::spawn(async move { server.handle_pub(socket).await });
+            tokio::spawn(async move {
+                if let Err(e) = server.handle_pub(socket).await {
+                    error!("Error while handling publisher: {}", e);
+                }
+            });
         }
     }
 
-    async fn handle_pub(&self, mut socket: TcpStream) {
+    async fn handle_pub(&self, mut socket: TcpStream) -> std::io::Result<()> {
+        let peer_addr = socket.peer_addr()?;
         let (reader, writer) = socket.split();
         let mut reader = BufReader::new(reader);
         let mut writer = BufWriter::new(writer);
         let mut key = String::new();
-        reader.read_line(&mut key).await.unwrap();
-        print!("PUB: {}", key);
+        reader.read_line(&mut key).await?;
+        info!("PUB: {} {}", peer_addr, key);
         let mut subscriber: TcpStream;
         {
             let mut subscribers = self.subscribers.lock().await;
             if let Some(s) = subscribers.remove(&key) {
                 subscriber = s;
             } else {
-                writer.write_all("ERROR: unknown subscriber".as_bytes()).await.unwrap();
-                writer.flush().await.unwrap();
-                return;
+                info!("Unknown subscriber: {}", key);
+                writer.write_all("ERROR: unknown subscriber\n".as_bytes()).await?;
+                writer.flush().await?;
+                return Ok(())
             }
         }
-        writer.write_all("OK\n".as_bytes()).await.unwrap();
-        writer.flush().await.unwrap();
-        println!("Starting copy");
+        writer.write_all("OK\n".as_bytes()).await?;
+        writer.flush().await?;
+        info!("Starting copy for {}", key);
         copy_bidirectional(&mut socket, &mut subscriber)
-            .await
-            .unwrap();
+            .await?;
+        Ok(())
     }
 }
